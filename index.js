fa658f47c66f10ec9a050413257f814c79f5a521
finetype <finesttype@gmail.com>
2015-12-06 21:24:03 -0600

    changed game logic to use -1,0,1 instead of 0,4,1 for representing game state

--- index.js
+++ index.js
@@ -42,10 +42,10 @@
 
   var Board = React.createClass({
     getInitialState: function() {
-      return {game: [[4,4,4], [4,4,4], [4,4,4]]}
+      return {game: [[0,0,0], [0,0,0], [0,0,0]]}
     },
     clearBoard: function() {
-      this.setState({game:[[4,4,4], [4,4,4], [4,4,4]]})
+      this.setState({game:[[0,0,0], [0,0,0], [0,0,0]]})
     },
     render: function() {
       var rows = [];
@@ -82,12 +82,12 @@
 
 
   var cellDic = {
-    0: "O",
-    1: "X",
-    4: "" ,
-    "O": 0,
+    '-1': "O",
+    '1': "X",
+    '0': "" ,
+    "O": -1,
     "X": 1,
-    "":  4
+    "":  0
   }
 
   ReactDOM.render(
@@ -101,29 +101,29 @@
     for (var i = 0; i < 3; i++) {
       // Check all collumns:
       var col = [ game[0][i], game[1][i], game[2][i] ].reduce((a,b) => a + b);
-      if (col === 0 || col === 3) {
-        alert("Winner is " + (col === 0 ? "O" : "X") + " on col " + (i+1) );
+      if (col === -3 || col === 3) {
+        alert("Winner is " + (col === 1 ? "X" : "O") + " on col " + (i+1) );
         return true;
       }
       
       // Check all rows:
       var row = [ game[i][0], game[i][1], game[i][2] ].reduce((a,b) => a + b);
-      if (row === 0 || row === 3) {
-        alert("Winner is " + (row === 0 ? "O" : "X") + " on row " + (i+1) );
+      if (row === -3 || row === 3) {
+        alert("Winner is " + (row === 1 ? "X" : "O") + " on row " + (i+1) );
         return true;
       }
     }
 
     // Check diagonals
     var diag1 = [ game[0][0], game[1][1], game[2][2] ].reduce((a,b) => a + b);
-    if (diag1 === 0 || diag1 === 3) {
-      alert("Winner is " + (diag1 === 0 ? "O" : "X") + " on diagonal from top left ");
+    if (diag1 === -3 || diag1 === 3) {
+      alert("Winner is " + (diag1 === 1 ? "X" : "O") + " on diagonal from top left ");
       return true;
     }
 
     var diag2 = [ game[0][2], game[1][1], game[2][0] ].reduce((a,b) => a + b);
-    if (diag2 === 0 || diag2 === 3) {
-      alert("Winner is " + (diag2 === 0 ? "O" : "X") + " on diagonal from top right");
+    if (diag2 === -3 || diag2 === 3) {
+      alert("Winner is " + (diag2 === 1 ? "X" : "O") + " on diagonal from top right");
       return true;
     }
 
